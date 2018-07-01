d6d5db23018a874ca9845d2e23436127ace45eb5
pokeron1 <35638754+pokeron1@users.noreply.github.com>
2018-07-01 20:52:13 +0100

    Changeable content
    added ability to change content with "back/forawrds" buttons or text. Currently set to be used on the single part (at bottom of test page) to showcase, but should be easy to scale. Possibly by adding seperate arrays of text, and calling which one you want as part of the function? possibly a dictionary, but I'm not sure if that works in JS

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
+var Text=1;
 function LinkTo(page) {
 	var body = document.getElementById("body");
 	body.classList.add("fade-out");
 	setTimeout(function(){window.location.href = page;}, 700)
+	Text=1
 }
+function Next(id){
+	Text=Text+1;
+	ChangeText(id);
+}
+function Back(id){
+	Text=Text-1;
+	ChangeText(id);
+}
+function ChangeText(id) {
+	switch (Text) {
+		case 0:
+			Text=1;
+			break;
+		case 1:
+			document.getElementById(id).innerHTML="I can't be bothered to type that all out again. You get the idea.";
+			break;
+		case 2:
+			document.getElementById(id).innerHTML="Heres the second page! Theres 2 more.";
+			break;
+		case 3:
+			document.getElementById(id).innerHTML="pretty easily scalable amirite?";
+			break;
+		case 4:
+			document.getElementById(id).innerHTML="There's no more, turn back, young one. I've turned looping off for now.";
+			break;
+		case 5:
+			Text=4;
+	}
+}
+	
